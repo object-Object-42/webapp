e4f4a077f0b060afa851401fd8379bc09b5ec194
Lukas Bug <mail@lukasbug.de>
2024-10-26 16:09:56 +0200

    added main route and prompts

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils
+from app.api.routes import items, login, users, utils, chat, vector, podcast
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+api_router.include_router(vector.router, prefix="/vector", tags=["vector"])
+api_router.include_router(podcast.router, prefix="/podcast", tags=["podcast"])
